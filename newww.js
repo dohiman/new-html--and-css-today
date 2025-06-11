3e0c96cf232b2d0a24c0558177a423955bb929a4
john chinedu <johndoehiman@gmail.com>
2025-06-11 14:07:11 +0100

    first commit

---
+++ newww.js
@@ -0,0 +1,18 @@
+function addUpto(x){
+
+
+    let result = 0;
+
+
+    for (let i = x; i > 0; i--) {
+        result=result+i;
+        
+    }
+while ()) {
+    
+}
+
+    console.log(result)
+}
+
+addUpto(6);
